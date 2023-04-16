9275099ea32756e7667d0a1e86f867c5502ab731
nisitin <seiya110307@icloud.com>
2023-04-16 19:55:54 +0900

    fix: とりあえずデザイン

--- UI/Button.tsx
+++ UI/Button.tsx
@@ -1,16 +1,18 @@
-"use client"
+'use client'
 import React from 'react'
 
 type Props = {
-  text: string;
-  color: string;
+  text: string
+  color: string
   children?: React.ReactNode
 }
 
-export const Button = React.memo(({ text, color = "bg-white", ...props  }: Props) => {
+export const Button = React.memo(({ text, color = 'bg-white', ...props }: Props) => {
   return (
-    <button>Button</button>
+    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+      {text}
+    </button>
   )
 })
 
-Button.displayName = "Button"
+Button.displayName = 'Button'
